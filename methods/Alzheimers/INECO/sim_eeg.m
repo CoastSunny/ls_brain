be24f30c950ba4960fb2d68aa92b7472d1a4170a
Loukianos Spyrou <lspyrou@pcidcom179.see.ed.ac.uk>
2016-10-11 15:51:17 +0100

    modified:   methods/Alzheimers/INECO/sim_eeg.m

--- methods/Alzheimers/INECO/sim_eeg.m
+++ methods/Alzheimers/INECO/sim_eeg.m
@@ -6,18 +6,18 @@ tr=50;
 clear j xf xch xep PC0 PC7 FC0 FC7
 Y=0;
 nsource=5;
-Options(1)=0.0001;
+Options=.001;
 for si=1:nsource
-    xf(:,si)=randn(freqs,1);%+j*randn(freqs,1);
+    xf(:,si)=randn(freqs,1)+j*randn(freqs,1);
     xch(:,si)=randn(ch,1)+j*randn(ch,1);
-    xep(:,si)=randn(ep,1);%+j*randn(ep,1);
+    xep(:,si)=randn(ep,1)+j*randn(ep,1);
     xtr(:,si)=exp(-j*rand(tr,1));
-%     xtr(:,si)=rand(tr,1);
+%      xtr(:,si)=rand(tr,1);
     if si<6
         for ti=1:128
             for tj=1:128
 %                 G{si}(ti,tj)=1/(abs(xch(ti,si))-abs(xch(tj,si)));
-                G{si}(ti,tj)=1/norm(angle(xch(ti,si)-xch(tj,si)))^2;
+                G{si}(ti,tj)=1/norm(angle(xch(ti,si))-angle(xch(tj,si)))^2;
 %                  G{si}(ti,tj)=1/norm(abs(xch(ti,si))-abs(xch(tj,si)));
                 
             end
@@ -34,10 +34,10 @@ for si=1:nsource
     S=tprod(temp,[1 2 3 -4],xtr(:,si),[4 -4]);
     Y=Y+S;
 end
-Y=Y+1*randn(freqs,ch,ep,tr);
+Y=Y+0*randn(freqs,ch,ep,tr);
 ncomps=5;
 
-[Fp,Ye,Ip,Exp,e,Rpen]=parafac_reg(Y,ncomps,G,Alpha(a),Options,[9 7 9 9]);
+[Fp,Ye,Ip,Exp,e,Rpen]=parafac_reg(Y,ncomps,G,Alpha(a),Options,[0 7 0 0]);
 % figure,plot(abs(Fp{3})./(ones(5,1)*abs(std(Fp{3},0,1))))
 % figure,plot(abs(xep)./(ones(5,1)*abs(std(xep,0,1))))
 
@@ -65,7 +65,7 @@ for jj=1:ncomps
     end
 end
 
-[Fp,Ye,Ip,Exp,e,Rpen]=parafac_reg(Y,ncomps,G,Alpha(a),Options,[9 0 9 0]);
+[Fp,Ye,Ip,Exp,e,Rpen]=parafac_reg(Y,ncomps,G,Alpha(a),Options,[0 0 0 0]);
 for jj=1:ncomps
     for ii=1:nsource
         
@@ -86,7 +86,7 @@ for jj=1:ncomps
         
     end
 end
-
+format short
 FC0,FC7
 PC0,PC7
-resgood(end+1,:)=[sum(diag(abs(PC7))) sum(diag(abs(FC7)))]/5
+resgood(end+1,:)=[mean(max(abs(PC0))) mean(diag(abs(PC7))) mean(max(abs(FC0))) mean(diag(abs(FC7)))]
