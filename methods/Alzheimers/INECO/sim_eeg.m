7221fad33a1abcdfa95797cc6c843c6cfd267e6d
Loukianos Spyrou <lspyrou@pcidcom179.see.ed.ac.uk>
2016-10-27 13:02:22 +0100

    modified:   methods/Alzheimers/INECO/INECO_tensorfact_complex_noep.m 	modified:   methods/Alzheimers/INECO/sim_eeg.m 	deleted:    methods/Alzheimers/INECO/niceplot.m~ 	deleted:    methods/Misc/nway330/parafac_reg.m~

--- methods/Alzheimers/INECO/sim_eeg.m
+++ methods/Alzheimers/INECO/sim_eeg.m
@@ -5,39 +5,30 @@ ep=5;
 tr=50;
 clear j xf xch xep PC0 PC7 FC0 FC7
 Y=0;
-nsource=5;
-Options=.001;
+nsource=10;
+Options=[];
+G=[];FC=[];PC=[];
 for si=1:nsource
-    xf(:,si)=randn(freqs,1)+j*randn(freqs,1);
-    xch(:,si)=randn(ch,1)+j*randn(ch,1);
-    xep(:,si)=randn(ep,1)+j*randn(ep,1);
-    xtr(:,si)=exp(-j*rand(tr,1));
-%      xtr(:,si)=rand(tr,1);
-    if si<6
-        for ti=1:128
-            for tj=1:128
-%                 G{si}(ti,tj)=1/(abs(xch(ti,si))-abs(xch(tj,si)));
-                G{si}(ti,tj)=1/norm(angle(xch(ti,si))-angle(xch(tj,si)))^2;
-%                  G{si}(ti,tj)=1/norm(abs(xch(ti,si))-abs(xch(tj,si)));
-                
-            end
-        end
-        % %         G{si}=sign(angle(xch(:,si))*angle(xch(:,si))');
-%         G{si}(isnan(G{si})==1)=1;
-        
-        G{si}(eye(128)==1)=0;
-%         G{si}=(weight_conversion(G{si},'normalize'));
-        
+    xf(:,si)=randn(freqs,1);
+    xch(:,si)=randn(ch,1);    
+    ph(:,si)=rand(tr,1);
+    xtr(:,si)=exp(-j*ph(:,si));
+    if si==5
+        xtr(:,si)=exp(-j*(ph(:,4)+ph(:,5)));
+    end
+    if si==2
+        xtr(:,si)=exp(-j*(ph(:,1)+ph(:,2)));
     end
     temp=tprod(xf(:,si),[1 -2],xch(:,si),[2 -2]);
-    temp=tprod(temp,[1 2 -3],xep(:,si),[3 -3]);
-    S=tprod(temp,[1 2 3 -4],xtr(:,si),[4 -4]);
+%     temp=tprod(temp,[1 2 -3],xep(:,si),[3 -3]); old
+    S=tprod(temp,[1 2 -3],xtr(:,si),[3 -3]);
     Y=Y+S;
 end
-Y=Y+0*randn(freqs,ch,ep,tr);
-ncomps=5;
+Y=Y+5*randn(freqs,ch,tr);
+ncomps=10;
 
-[Fp,Ye,Ip,Exp,e,Rpen]=parafac_reg(Y,ncomps,G,Alpha(a),Options,[0 8 0 0]);
+[Fp,Ye,Ip,Exp,e,Rpen]=parafac_reg(Y,ncomps,[],[],Options,[9 9 0]);
+Fp{1}=Fp;
 % figure,plot(abs(Fp{3})./(ones(5,1)*abs(std(Fp{3},0,1))))
 % figure,plot(abs(xep)./(ones(5,1)*abs(std(xep,0,1))))
 
@@ -47,46 +38,23 @@ ncomps=5;
 for jj=1:ncomps
     for ii=1:nsource
         
-        x=angle(Fp{2}(:,jj));
+        x=angle(Fp{1}{2}(:,jj));
         y=angle(xch(:,ii));
         temp=corrcoef(x,y);
-        FC7(jj,ii)=temp(1,2);
+        FC(jj,ii)=temp(1,2);
         
     end
 end
 for jj=1:ncomps
     for ii=1:nsource
         
-        x=abs(Fp{2}(:,jj));
+        x=abs(Fp{1}{2}(:,jj));
         y=abs(xch(:,ii));
         temp=corrcoef(x,y);
-        PC7(jj,ii)=temp(1,2);
+        PC(jj,ii)=temp(1,2);
         
     end
 end
-
-[Fp,Ye,Ip,Exp,e,Rpen]=parafac_reg(Y,ncomps,G,Alpha(a),Options,[0 0 0 0]);
-for jj=1:ncomps
-    for ii=1:nsource
-        
-        x=angle(Fp{2}(:,jj));
-        y=angle(xch(:,ii));
-        temp=corrcoef(x,y);
-        FC0(jj,ii)=temp(1,2);
-        
-    end
-end
-for jj=1:ncomps
-    for ii=1:nsource
-        
-        x=abs(Fp{2}(:,jj));
-        y=abs(xch(:,ii));
-        temp=corrcoef(x,y);
-        PC0(jj,ii)=temp(1,2);
-        
-    end
-end
-format short
-FC0,FC7
-PC0,PC7
-resgood(end+1,:)=[mean(max(abs(PC0))) mean(diag(abs(PC7))) mean(max(abs(FC0))) mean(diag(abs(FC7)))]
+%FC,
+PC
+% resgood(end+1,:)=[mean(max(abs(PC0))) mean(diag(abs(PC7))) mean(max(abs(FC0))) mean(diag(abs(FC7)))]
