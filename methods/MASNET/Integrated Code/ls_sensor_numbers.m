33f21e57440baaa6d4b695ed2eb8d682cdd63ebb
Loukianos Spyrou <lspyrou@pcidcom179.see.ed.ac.uk>
2017-12-05 16:06:57 +0000

    hello

--- methods/MASNET/Integrated Code/ls_sensor_numbers.m
+++ methods/MASNET/Integrated Code/ls_sensor_numbers.m
@@ -1,13 +1,18 @@
 function ls_sensor_numbers(fl)
 
 
+if isunix==0
+    pth='C:\Users\Loukianos\Documents\MATLAB\ls_brain\results\masnet\'
+else
+    pth='~/Documents/ls_brain/results/masnet/';
+end
+
 if nargin<1
-    cd 'C:\Users\Loukianos\Documents\MATLAB\ls_brain\results\masnet\snr'
+    cd([pth 'snr/'])
     uiopen;
 else
    load(fl) 
 end
-
 parsecfg
 % Speed of light, wavelength
 c = 3e8;
@@ -29,6 +34,7 @@ for pfa_idx=1:numel(pfa)
             p_all = calculating_Prob_detection_v2(AC_sample,Td,Tc,ALL_snr(:,:,:,idx,:),pfa(pfa_idx));
             p_mean = calculating_Prob_detection_v2(AC_sample,Td,Tc,mean(ALL_snr(:,:,:,idx,:),4),pfa(pfa_idx));
             p_sum= calculating_Prob_detection_v2(AC_sample,Td,Tc,sum(ALL_snr(:,:,:,idx,:),4),pfa(pfa_idx));
+           
 
             % get average performance over all target locations and shadowing
             p_all_s(j,:) = mean(p_all(:)); 
@@ -43,7 +49,7 @@ for pfa_idx=1:numel(pfa)
             s_mean_s(j,:)  = std(p_mean(:));  % get average performance over sensors @@
             p_sum_s(j,:)   = mean(p_sum(:));  % get average performance over sensors @@
             s_sum_s(j,:)   = std(p_sum(:));   % get average performance over sensors @@
-            p_bsens_s_old(j,:) = sum(bsens(:)==1)/numel(bsens(:));
+            p_bsens_s_old(j,:) = sum(bsens(:)==.9)/numel(bsens(:));
 %             if i>=4            
 %                 snr_3best=getNbest(ALL_snr(:,:,:,idx),3);
 %                 p_3best = calculating_Prob_detection_v2(AC_sample,Td,Tc,sum(snr_3best,4),pfa(pfa_idx));
@@ -52,7 +58,19 @@ for pfa_idx=1:numel(pfa)
 %                 p_3best_d(j,:) = mean(mean(mean(p_3best)));
 %             end
             
-                
+            [Pe_BPSK,Pe_QPSK,Pe_4AM,Pe_8AM] = calculating_BER(ALL_snr(:,:,:,idx,:)); 
+            b_bpsk=min(Pe_BPSK,[],4);
+            b_qpsk=min(Pe_QPSK,[],4);
+            b_4am=min(Pe_4AM,[],4);
+            b_8am=min(Pe_8AM,[],4);
+            eb_bpsk_s(j,:) = mean(b_bpsk(:));   
+            seb_bpsk_s(j,:) = std(b_bpsk(:));
+            eb_qpsk_s(j,:) = mean(b_qpsk(:));   
+            seb_qpsk_s(j,:) = std(b_qpsk(:));
+            eb_4am_s(j,:) = mean(b_4am(:));  
+            seb_4am_s(j,:) = std(b_4am(:));
+            eb_8am_s(j,:) = mean(b_8am(:));  
+            seb_8am_s(j,:) = std(b_8am(:));
             
         end
         
@@ -67,11 +85,20 @@ for pfa_idx=1:numel(pfa)
         psum_av(i,pfa_idx)=mean(p_sum_s);
         psum_std(i,pfa_idx)=mean(s_sum_s);
         
+        eb_bpsk_av(i,pfa_idx)=mean(eb_bpsk_s);
+        eb_bpsk_std(i,pfa_idx)=mean(seb_bpsk_s);
+        eb_qpsk_av(i,pfa_idx)=mean(eb_qpsk_s);
+        eb_qpsk_std(i,pfa_idx)=mean(seb_bpsk_s);
+        eb_4am_av(i,pfa_idx)=mean(eb_4am_s);
+        eb_4am_std(i,pfa_idx)=mean(seb_4am_s);
+        eb_8am_av(i,pfa_idx)=mean(eb_8am_s);
+        eb_8am_std(i,pfa_idx)=mean(seb_8am_s);
+                                
 %         if i>=4;p3best_d(i,pfa_idx)=mean(mean(p_3best_d));end;
     end
 end
 
-filetosave = [ 'C:\Users\Loukianos\Documents\MATLAB\ls_brain\results\masnet\probs\' 'Pr_lte_' filename '_Time_' num2str(Time_samples)...
+filetosave = [ pth 'probs/' 'Pr_ofdm_' filename '_Time_' num2str(Time_samples)...
         '_TS_' num2str(Type_Scenario)...
         '_TE_' num2str(Type_Environment)...
         '_Num_Sensors_' num2str(Num_sensors)...
@@ -79,7 +106,9 @@ filetosave = [ 'C:\Users\Loukianos\Documents\MATLAB\ls_brain\results\masnet\prob
         'dBW_sigma_' num2str(sigm) 'dB.mat'];
  %filename3 = ['~/Documents/projects/ls_brain/results/masnet/probs/test.mat'];
  save(filetosave,'pall_av','pall_std','pmean_av','pmean_std','psum_av','psum_std',...
-     'pbsens_av','pbsens_std','pbsens_noloc_std','pbsens_av_old'); 
+     'pbsens_av','pbsens_std','pbsens_noloc_std','pbsens_av_old'...
+     ,'eb_bpsk_av','eb_qpsk_av','eb_4am_av','eb_8am_av'...
+     ,'eb_bpsk_std','eb_qpsk_std','eb_4am_std','eb_8am_std'); 
 
 
 end
